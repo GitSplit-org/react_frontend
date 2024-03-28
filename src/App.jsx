4df6a0036c57af91c984d4ca9af61de8fc7e6a35
Punarv Pawade <punarvpawade17@gmail.com>
2024-03-28 12:20:32 +0530

    add thirdweb

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,27 @@ import AddProject from "./pages/AddProject";
 import OpenProject from "./pages/openProject"; // corrected the import path
 
 import Projects from "./pages/projects"; // corrected the component name
+import {
+  ThirdwebProvider,
+  metamaskWallet,
+  coinbaseWallet,
+  walletConnect,
+} from "@thirdweb-dev/react";
 
 function App() {
   return (
     <>
-  
-     
+      <ThirdwebProvider
+        activeChain="ethereum"
+        clientId="1971b4ffc7b4410e350ba34c8694d7df"
+        supportedWallets={[
+          metamaskWallet({
+            recommended: true,
+          }),
+          coinbaseWallet(),
+          walletConnect(),
+        ]}
+      >
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />} />
@@ -18,7 +33,7 @@ function App() {
             <Route path="/openproject" element={<OpenProject />} />
           </Routes>
         </BrowserRouter>
-   
+      </ThirdwebProvider>
     </>
   );
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,5 @@
 // Import React if not already imported
-
+import { ConnectWallet } from "@thirdweb-dev/react";
 const Navbar = () => {
   return (
     <>
@@ -39,12 +39,15 @@ const Navbar = () => {
                 </a>
               </li>
               <li className="flex items-center">
-                <a
+                {/* <a
                   href="#"
                   className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Support a project
-                </a>
+                </a> */}
+                <div>
+                  <ConnectWallet />
+                </div>
               </li>
             </ul>
           </div>
